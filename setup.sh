533c567838eeb15ae78549f5a798d46725c8c40a
Paolo <39755290+AlexPaolo@users.noreply.github.com>
2024-10-01 15:59:42 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-mkdir -p data
-mkdir newfolder
-pip install cassandra-driver
+mkdir ny_taxi_cassandra
+docker run --name=cass-docker -p 9042:9042 -e CASSANDRA_USER=cassandra -e CASSANDRA_PASSWORD=admin123 -v $(pwd)/ny_taxi_cassandra:/var/lib/cassandra -d cassandra
+code-insiders ./main.py
