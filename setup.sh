fd1748b5de0b8389beb14ae35b3488c3c1110da9
Paolo <39755290+AlexPaolo@users.noreply.github.com>
2024-10-01 16:03:59 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 mkdir ny_taxi_cassandra
 docker run --name=cass-docker -p 9042:9042 -e CASSANDRA_USER=cassandra -e CASSANDRA_PASSWORD=admin123 -v $(pwd)/ny_taxi_cassandra:/var/lib/cassandra -d cassandra
-code-insiders ./main.py
+code ./main.py
